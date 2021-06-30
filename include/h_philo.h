bd1a03739a23c534c6590ebd8ee737d599e67cba
adrien <adrien.constan@gmail.com>
2021-06-30 12:21:05 +0200

    auto-commit

--- include/h_philo.h
+++ include/h_philo.h
@@ -6,7 +6,7 @@
 /*   By: calao <adconsta@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 15:29:50 by calao             #+#    #+#             */
-/*   Updated: 2021/06/24 19:21:21 by calao            ###   ########.fr       */
+/*   Updated: 2021/06/30 12:10:15 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,27 @@
 # include <pthread.h>
 # include <sys/time.h>
 
-typedef	struct s_philo
+typedef	struct	s_philo
 {
-	int			fork_philo_nb;
+	pthread_t	philo;
+	int			l_fork;
+	int			r_fork;
+	int			p_sleep;
+	int			p_eat;
+	int			p_death;
+	int			p_meal;
+}				t_philo;
+
+
+typedef	struct s_data
+{
+	int			philo_nb;
+	int			*fork_tab;
 	int			meal_nb;
 	int			death;
 	int			eat;
 	int			sleep;
-	pthread_t	*philo;
-}				t_philo;
-
+	t_philo		*philo_tab;
+}				t_data;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,13 @@
 #include "h_philo.h"
 
 int		parsing_error(char **argv);
-int		ft_philo_atoi(char *str);
-void	ft_set_data(t_philo *data, char **argv, int argc);
+int		ft_data_atoi(char *str);
+int		ft_set_data(t_data *data, char **argv, int argc);
+void	ft_free_philo(t_data *data);
 
-void	print_philo(t_philo *data)
+void	print_data(t_data *data)
 {
-	printf("Number of philosophers: %d \n", data->fork_philo_nb);
+	printf("Number of philosophers: %d \n", data->philo_nb);
 	printf("Lifetime : %d ms\n", data->death);
 	printf("Time to eat : %d ms\n", data->eat);
 	printf("Time to sleep : %d ms\n", data->sleep);
@@ -33,7 +34,7 @@ int		ft_atoi(char *str)
 
 int		main(int argc, char **argv)
 {
-	t_philo	data;
+	t_data	data;
 
 	if (argc < 5 || argc > 6)
 	{
@@ -45,14 +46,26 @@ int		main(int argc, char **argv)
 		printf("Error: parsing: wrong args\n");
 		return (1);
 	}
-	ft_set_data(&data, argv, argc);
-	print_philo(&data);
+	if (ft_set_data(&data, argv, argc) != 0)
+		return (-1);
+	print_data(&data);
+	ft_free_philo(&data);
 	return (42);
 }
 
-void	ft_set_data(t_philo *data, char **argv, int argc)
+void	ft_free_philo(t_data *data)
 {
-	data->fork_philo_nb = ft_atoi(argv[1]);
+	if (data->fork_tab != NULL)
+		free(data->fork_tab);
+	if (data->philo_tab != NULL)
+		free(data->philo_tab);
+}
+
+int		ft_set_data(t_data *data, char **argv, int argc)
+{
+	int i;
+	
+	data->philo_nb = ft_atoi(argv[1]);
 	data->death = ft_atoi(argv[2]);
 	data->eat = ft_atoi(argv[3]);
 	data->sleep = ft_atoi(argv[4]);
@@ -60,8 +73,36 @@ void	ft_set_data(t_philo *data, char **argv, int argc)
 		data->meal_nb = ft_atoi(argv[5]);
 	else
 		data->meal_nb = -1;
+	data->fork_tab = malloc(sizeof(int) * (data->philo_nb));
+	data->philo_tab = malloc(sizeof(t_data) * (data->philo_nb));
+	if (data->fork_tab == NULL || data->philo_tab == NULL)
+		return (1);
+	i = 0;
+	while (i < data->philo_nb)
+	{
+		data->fork_tab[i] = i;
+		data->philo_tab[i].r_fork = i;
+		if (i == 0)
+			data->philo_tab[i].l_fork = data->philo_nb - 1;
+		else
+			data->philo_tab[i].l_fork = i - 1;
+		printf("fork_tab[%d] = %d; ", i, data->fork_tab[i]);
+		i++;
+	}
+	printf("\n");
+	/*
+	i = 0;
+	while (i < data->philo_nb)
+	{
+		printf("LEFT_fork = %d <-- philo[ %d ] --> RIGHT_f = %d\n", data->philo_tab[i].l_fork, i, data->philo_tab[i].r_fork);
+		i++;
+	}
+	printf("\n");
+	*/
+	return (0);
 }
-int		ft_philo_is_digit(char *str)
+
+int		ft_data_is_digit(char *str)
 {
 	int i;
 	
@@ -77,7 +118,6 @@ int		ft_philo_is_digit(char *str)
 	return (0);
 }
 
-
 int		parsing_error(char **argv)
 {
 	int i;
@@ -85,11 +125,9 @@ int		parsing_error(char **argv)
 	i = 1;
 	while (argv[i])
 	{
-		if (ft_philo_is_digit(argv[i]))
+		if (ft_data_is_digit(argv[i]))
 			return (1);
 		i++;
 	}
 	return (0);
 }
-
-
